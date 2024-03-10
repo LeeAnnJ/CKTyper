9d4e7f280257ab878bd502c1a57fe91d3d6d7d38
LeeAnnJ <lee_ann.j@qq.com>
2024-03-10 16:47:43 +0800

    modify input text processing in summarizer

--- code/Generate_Question/combine_prompt.py
+++ code/Generate_Question/combine_prompt.py
@@ -20,36 +20,38 @@ class PromptCombiner(object):
     def add_comments(self, text, comments, sum, level):
         for com in comments:
             com_text = self.summarizer.preprocess_body(com["Body"], level)
-            if sum and len(com_text)>200:
+            if sum and len(com_text[0])>200:
                 com_text = self.summarizer.generate_summary_pegasus(com_text)
+            else:
+                com_text = "".join(com_text)
             if len(com_text)>0: text += com_text
         return text
 
 
+    def gen_prompt_single_part(self, part, sum:bool, with_comments:bool, level):
+        body = part["Body"]
+        text = self.summarizer.preprocess_body(body,level)
+        if sum and len(text[0])>200:
+            text = self.summarizer.generate_summary_pegasus(text)
+        else:
+            text = "".join(text)
+        if with_comments: # add comments text
+            comments = part["Comments"]
+            text = self.add_comments(text,comments,sum)
+        return text
+
     # combine code snippet,text in question, answer and comments
-    def generate_prompt_singal_post(self, post, sum:bool, with_ans:bool, with_comments:bool, level:int):
+    def gen_prompt_singal_post(self, post, sum:bool, with_ans:bool, with_comments:bool, level):
         # get question text
         question = post["Question"]
         ques_title = question["Title"]
-        ques_body = question["Body"]
-        ques_text = self.summarizer.preprocess_body(ques_body,level)
-        if sum and len(ques_text)>200:
-            ques_text = self.summarizer.generate_summary_pegasus(ques_text)
-        if with_comments: # add comments text
-            ques_comments = question["Comments"]
-            ques_text = self.add_comments(ques_text,ques_comments,sum)
+        ques_text = self.gen_prompt_single_part(question, sum, with_comments, level)
 
         if with_ans: # get answer texts
             answers = post["Answers"]
             ans_texts = ""
             for answer in answers:
-                ans_body = answer["Body"]
-                ans_text = self.summarizer.preprocess_body(ans_body,level)
-                if sum and len(ans_text)>200:
-                    ans_text = self.summarizer.generate_summary_pegasus(ans_text)
-                if with_comments: # add comments text
-                    ans_comments = answer["Comments"]
-                    ans_text = self.add_comments(ans_text,ans_comments,sum)
+                ans_text = self.gen_prompt_single_part(answer, sum, with_comments, level)
                 ans_texts += ans_text
             prompt = ques_title + ques_text + ans_texts
         else:
@@ -63,7 +65,7 @@ class PromptCombiner(object):
         for post_file in post_list:
             post = utils.load_json(post_file)
             self.logger.info(f"generate prompt for post: {post_file}")
-            prompt = self.generate_prompt_singal_post(post, summarize, ans, with_comments, level)
+            prompt = self.gen_prompt_singal_post(post, summarize, ans, with_comments, level)
             prompt_list.append(prompt)
         return prompt_list
 
--- code/Generate_Question/summarize.py
+++ code/Generate_Question/summarize.py
@@ -2,6 +2,7 @@ import re
 import os
 import sys
 import torch
+from nltk import sent_tokenize
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -19,8 +20,8 @@ class TextSummarizer(object):
         self.model.to(device)
         pass
     
-    # remove code and tags from body
-    def preprocess_body(self,body,level=0):
+    # remove code and tags from body, and split text into small pieces
+    def preprocess_body(self,body,level:int=0)->list[str]:
         if level > 0: # remove code from body
             codes = re.findall(r'<code>(.*?)</code>',body,re.DOTALL)
             for code in codes: # remove codes from body
@@ -28,18 +29,34 @@ class TextSummarizer(object):
                 # deleted = "<pre><code>"+code+"</code></pre>"
                 body = body.replace(code, '')
         body = re.sub(r'<.*?>','',body,flags=re.DOTALL) # remove tags from body, e.g <p>, <strong>
-        if level > 1: # keep sentences containing API elements
+        # split text into small pieces
+        splited_body = []
+        sentences = sent_tokenize(text)
+        part = ""
+        part_len = 0
+        for sentence in sentences:
+            if level > 1: # keep sentences containing API elements
             # 什么样的单词是api元素?
-            pass
-        return body
+                pass
+            part += sentence
+            part_len += len(sentence)
+            if part_len > self.size:
+                splited_body.append(part)
+                part = ""
+                part_len = 0
+        if len(part)>0:
+            if len(part)<self.size/2 and len(splited_body)>0:
+                splited_body[-1] += part
+            else: splited_body.append(part)
+        return splited_body
     
     def split_text(self,text): # ensure text's length is less than model's max length
         return [text[i:i+self.size] for i in range(0, len(text), self.size)]
     
     # input: unprocessed body; output: summary of text
-    def generate_summary_pegasus(self, text): 
+    def generate_summary_pegasus(self, splited_text:list[str]): 
         summary = ""
-        splited_text = self.split_text(text)
+        # splited_text = self.split_text(text)
         for input in splited_text:
             input_ids = self.tokenizer.encode(input, return_tensors="pt", max_length=512, truncation=True)
             input_ids = input_ids.to(self.model.device)
