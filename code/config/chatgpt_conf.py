1e456e0024ef0e20838ce5f3614f82ad138aa2e6
LeeAnnJ <lee_ann.j@qq.com>
2025-02-10 19:34:21 +0800

    update data for extended evaluation

--- code/config/chatgpt_conf.py
+++ code/config/chatgpt_conf.py
@@ -1,5 +1,6 @@
-PROXY_URL = "http://xxx.xxx.xxx.xxx:xxxx" # or None
-MODEL = "gpt-3.5-turbo-0125"
+PROXY_URL = None #"http://xxx.xxx.xxx.xxx:xxxx" # or None
+MODEL = 'gpt-3.5-turbo-0125' #gpt-4o-mini-2024-07-18' or 'gpt-3.5-turbo-0125'
+
 # for ModelAccesser_V2
 # api key & base url for openai
 # better have more than one account to avoid the rate limit
@@ -14,8 +15,3 @@ ACCOUNTS = [
 
     }
 ]
-
-# for ModelAccesser_V1
-# cookie: __Secure-next-auth.session-token available from https://chat.openai.com/
-# better have more than one account to avoid the rate limit
-SESSION_TOKEN = ["",""]
